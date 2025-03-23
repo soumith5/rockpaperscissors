8c27bc1e1171787f4a9539bb26b4e0c7ac241c42
unknown <Soumithmandha146@gmail.com>
2025-03-23 19:37:23 +0530

    check

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // ROCK PAPER SCISSORS
-  const choices={"rock","paper"," scissors"};
+  const choices=["rock","paper"," scissors"];
   const playerDisplay=document.getElementById("playerDisplay");
   const computerDisplay=document.getElementById("computerDisplay");
   const resultDisplay=document.getElementById("resultDisplay");
@@ -11,22 +11,22 @@
 
 
   function playGame(playerChoice){
-   const computerChoice=chice[Math.floor(Math.random()* 3)];
+   const computerChoice=choices[Math.floor(Math.random()* 3)];
    let result="";
-   if(playerChoice===cmputerChoice){
+   if(playerChoice===computerChoice){
       result="IT'S A TIE";
    }
    else{
     switch(playerChoice){
         case "rock":
-            result=(computerChoice==="scissors")?"YOU WIN!":"YOU LOSE!";
-             break;
-             case"paper":
-             result=(compuChoice === "rock")?"YOU WIN":"YOU LOSE!";
-             break;
-             case "scissors":
-                result = (computerChoices === "paper")? " YOU WIN!":"YOU LOSE!";
-                break;
+        result=(computerChoice==="scissors")?"YOU WIN!":"YOU LOSE!";
+        break;
+        case"paper":
+        result=(computerChoice === "rock")?"YOU WIN":"YOU LOSE!";
+        break;
+        case "scissors":
+        result = (computerChoice === "paper")? " YOU WIN!":"YOU LOSE!";
+        break;
     }
    }
        playerDisplay.textContent=`PLAYER: ${playerChoice}`;
@@ -34,12 +34,12 @@
        resultDisplay.textContent=result;
        resultDisplay.classList.remove("greenText","redText");
         switch(result){
-             case "YOU WIN!";
+             case "YOU WIN!":
              resultDisplay.classList.add("greenText");
              playerScore++;
              playerScoreDisplay.textContent=playerScore;
              break;
-             case "YOU LOSE!";
+             case "YOU LOSE!":
              resultDisplay.classList.add("redText");
              computerScore++;
              computerScoreDisplay.textContent=computerScore;
